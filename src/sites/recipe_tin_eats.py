7b37e2ef66d7a80bd6a425f759f79cc9a67614d4
Luiz Maurício <2003lmvp@gmail.com>
2025-06-19 18:39:05 -0300

    Ingredients ready

--- src/sites/recipe_tin_eats.py
+++ src/sites/recipe_tin_eats.py
@@ -42,8 +42,8 @@ class RecipeTinEats(RecipeScraper):
         title = ''
         author = '' 
         prep_time = '' 
-        ingredients = ''
-        instructions = ''
+        ingredients = []
+        instructions = []
 
         recipe_html = self.soup.find_all("div", class_="wprm-recipe-recipe-tin-eats")
 
@@ -66,7 +66,36 @@ class RecipeTinEats(RecipeScraper):
         try:
             prep_time = prep_time.text.strip()
         except:
-            prep_time = "ERROR"                
+            prep_time = "ERROR"
+
+        grupos_de_ingredientes = recipe_html[0].select("ul", class_="wprm-recipe-ingredient-group")
+
+        for i, grupo in enumerate(grupos_de_ingredientes):
+            # Lista para guardar os ingredientes apenas DESTE grupo
+            ingredientes_do_grupo = []
+
+            itens_li = grupo.find_all('li', class_='wprm-recipe-ingredient')
+            
+
+            # --- PASSO 4: Loop Interno (para cada item/linha de ingrediente) ---
+            for item in itens_li:
+                # Extrai a quantidade
+                amount_tag = item.find("span", class_="wprm-recipe-ingredient-amount")
+                amount = amount_tag.text.strip() if amount_tag else ""
+                
+                name_tag = item.find("span", class_="wprm-recipe-ingredient-name")
+                name = name_tag.text.strip() if name_tag else ""
+                
+                # Junta tudo em uma string bonita e adiciona à lista do grupo
+                ingrediente_completo = f"{amount} {name}".strip()
+                if ingrediente_completo: # Só adiciona se não estiver vazio
+                    ingredientes_do_grupo.append(ingrediente_completo)
+
+            # Adiciona a lista de ingredientes deste grupo ao nosso dicionário principal
+            ingredientes_do_grupo_str = ', '.join(ingredientes_do_grupo)
+            if ingredientes_do_grupo_str != "":
+                ingredients.append(ingredientes_do_grupo_str)
+            
 
         return title, author, prep_time, ingredients, instructions
         
@@ -92,7 +121,6 @@ class RecipeTinEats(RecipeScraper):
                 recipe['INGREDIENTS'] = ingredients
                 recipe['INSTRUCTIONS'] = instructions
             self.url = url_tmp
-            print("PREP TIME3: ", recipe['PREP_TIME'])
             self.recipes.extend(recipe_list)
             print(numero_pagina_int)
             
