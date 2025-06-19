6fccf618831cda2b9dbfc4b29b21f4c6826caece
Eduardo Cabral <educcabral2002@gmail.com>
2025-06-19 18:53:01 -0300

    add recipe_tin_eats instructions

--- src/sites/recipe_tin_eats.py
+++ src/sites/recipe_tin_eats.py
@@ -69,7 +69,6 @@ class RecipeTinEats(RecipeScraper):
             prep_time = "ERROR"
 
         grupos_de_ingredientes = recipe_html[0].select("ul", class_="wprm-recipe-ingredient-group")
-
         for i, grupo in enumerate(grupos_de_ingredientes):
             # Lista para guardar os ingredientes apenas DESTE grupo
             ingredientes_do_grupo = []
@@ -96,6 +95,28 @@ class RecipeTinEats(RecipeScraper):
             if ingredientes_do_grupo_str != "":
                 ingredients.append(ingredientes_do_grupo_str)
             
+        # --- EXTRACTION OF INSTRUCTIONS ---
+        grupos_de_instrucoes = recipe_html[0].select("ul", class_="wprm-recipe-instruction-group")
+        for i, grupo in enumerate(grupos_de_instrucoes):
+            # Lista para guardar as instruções apenas DESTE grupo
+            instrucoes_do_grupo = []
+
+            itens_li = grupo.find_all('li', class_='wprm-recipe-instruction')
+            
+            # --- Loop Interno (para cada item/linha de instrução) ---
+            for item in itens_li:
+                # Extrai o texto da instrução
+                instruction_text_tag = item.find("div", class_="wprm-recipe-instruction-text")
+                instruction_text = instruction_text_tag.text.strip() if instruction_text_tag else ""
+                
+                # Adiciona à lista do grupo se não estiver vazio
+                if instruction_text: # Só adiciona se não estiver vazio
+                    instrucoes_do_grupo.append(instruction_text)
+
+            # Adiciona a lista de instruções deste grupo ao nosso dicionário principal
+            instrucoes_do_grupo_str = ', '.join(instrucoes_do_grupo)
+            if instrucoes_do_grupo_str != "":
+                instructions.append(instrucoes_do_grupo_str)
 
         return title, author, prep_time, ingredients, instructions
         
